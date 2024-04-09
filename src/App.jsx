a5da55770b78f0ae50c1d9ab13b77ba93a924f28
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-04-09 15:24:22 +0700

    Add player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import Player from "./components/Player"
 function App() {
-  
+
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Khoa" symbol="X"/>
+          <Player  name="John" symbol="O"/>
+        </ol>
+      </div>
+    </main>
   )
 }
 
